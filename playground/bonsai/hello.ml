043b86e830bba4ef53616d7e64be745d9feda04f
Arul Madhavan <arulselvan1234@gmail.com>
2020-03-15 00:34:54 -0700

    Add bonsai example

---
+++ playground/bonsai/hello.ml
@@ -0,0 +1,6 @@
+open Bonsai_web
+
+let component = Bonsai.const (Vdom.Node.text "Hello World")
+
+let (_: _ Start.Handle.t) =
+  Start.start_standalone ~initial_input:() ~initial_model:() ~bind_to_element_with_id:"app" component
